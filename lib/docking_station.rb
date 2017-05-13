7d1a0557212af27ceebdd892eb680ba3b4c9f9ec
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-13 19:44:46 +0100

    13 - UT(pass): Raise error when docking station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,15 +4,14 @@ class DockingStation
 
   attr_reader :bike
 
+  def dock(bike)
+    fail 'Docking station full' if @bike
+    @bike = bike
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
   end
-
-  def dock(bike)
-    # we need to return the bike we dock
-    @bike = bike
-  end
-
 
 end
