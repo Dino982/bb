db1524387674bcae8150df8d0988ca2bba0f8fed
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-13 20:26:47 +0100

    14 - UT(pass): Raise error when docking station reaches capacity of 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,20 +6,6 @@ describe DockingStation do
 
   it{ is_expected.to respond_to(:dock).with(1).argument}
 
-  it {is_expected.to respond_to(:bike)}
-
-  it 'docks something' do
-    bike = Bike.new
-    #we want to return the bike we docks
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    # again we want to return the bike we just docked
-    expect(subject.bike).to eq bike
-  end
 
   describe '#release_bike' do
     # use of # before method name implies its an instance method
